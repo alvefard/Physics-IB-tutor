5d43e9dd076703f9a9a533e3362757955d7d7776
Jhon Jairo Galán Rincón <49375319+alvefard@users.noreply.github.com>
2026-04-01 19:54:18 -0500

    Add files via upload

--- cliente_openai.py
+++ cliente_openai.py
@@ -4,7 +4,7 @@ from openai import OpenAI
 import os
 
 # Correcto: obtiene la variable de entorno
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 def preguntar_chatgpt(mensaje_usuario, modelo="gpt-4o-mini"):
     response = client.chat.completions.create(
